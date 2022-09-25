a79ea28b85c5c2dad4a1731cd96373b177aa7b8d
ART89-prog <dykpgilkom@mail.ru>
2022-09-25 22:52:04 +0300

    Добавил проект

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+$(() => {
+
+    $('header .btn').on('click', function() {
+        let href = $(this).attr('href');    
+        $('html, body').animate({
+            scrollTop: $(href).offset().top
+        }, {
+            duration: 870,  
+            easing: "linear" 
+        });
+    
+        return false;
+    });
+
+
+
+
+
+})
