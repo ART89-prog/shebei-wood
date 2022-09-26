1a95874a4cc0cc996f25ae5cb05746f01576669d
ART89-prog <72185684+ART89-prog@users.noreply.github.com>
2022-09-26 11:37:40 +0300

    Сделал

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 $(() => {
 
-    $('header .btn').on('click', function() {
+    $('.btn').on('click', function() {
         let href = $(this).attr('href');    
         $('html, body').animate({
             scrollTop: $(href).offset().top
